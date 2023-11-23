425aaf0060066408ff26bcd37c62d8b07cae0fd8
drakenevadie19 <drakennievadje19@gmail.com>
2023-11-23 13:01:10 -0600

    First Change: Build up base web

---
+++ my-blog/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome Blog</h1>
+      <div id="page-body">Welcome to my Blog!</div>
+    </div>
+  );
+}
+
+export default App;
