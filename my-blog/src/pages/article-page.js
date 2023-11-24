2243121273cf6814a40eec290758b397b1025f66
drakenevadie19 <drakennievadje19@gmail.com>
2023-11-24 17:18:51 -0600

    New Change: Create storage of fake article to display in article-content.js file, and try displaying them in article page

--- my-blog/src/pages/article-page.js
+++ my-blog/src/pages/article-page.js
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
+    //get the params from URL params
+    const {articleId} = useParams();
+    const article = articles.find(article => article.name === articleId);
+
     return (
-        <h1>This is Article Page!</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map(paragraph => (
+                <p>{paragraph}</p>
+            ))}
+        </>
+        
     )
 }
 
