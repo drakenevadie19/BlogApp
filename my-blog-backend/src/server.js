84b1184a265b87aec8d2375f6fe6eb1256234d23
drakenevadie19 <drakennievadje19@gmail.com>
2023-11-25 03:51:46 -0600

    New Change: Create upvote counting and response/request contact between user and server

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,4 +1,4 @@
-// //write all server code 
+/// /write all server code 
 // // Simple server to send us a message when we send a message to it 
 // import express from 'express';
 
@@ -27,22 +27,73 @@
 // //Then, we will use NodeJS to run our express server, using node src/server.js
 // //Then we go to the link http://localhost:8000/hello, server will response "Hello!"
 
+// import express from 'express';
+
+// const app = express();
+// //MIDDLEware
+// //Tell express server: when it receives a request that has a JSON body/payload, 
+// // it is going to parse and automatically make that available to us on req.body
+// app.use(express.json())
+
+// app.get('/hello/:name', (req, res) => {
+//     //params give us URL params here => name is a URL param here
+//     // our URL can have more than 1 URL param
+//     const {name} = req.params;
+//     res.send(`Hello ${name}!!!`);
+// })
+
+// app.post('/hello', (req, res) => {
+//     console.log(req.body)
+//     res.send(`Hello ${req.body.name}!`);
+// })
+
+// app.listen(8000, () => {
+//     console.log("Server is listening on port 8000")
+// })
+
+//What we want to do here is that users can upvote our articles to help our articles is the most popular/helpful
 import express from 'express';
 
+//temporaty fake db, which contain fake upvote 
+// Later, we will replace this with MongoDB
+let articlesInfo = [
+    {
+        name: 'learn-react',
+        upvotes: 0
+    },{
+        name: 'learn-node',
+        upvotes: 0
+    },{
+        name: 'mongodb',
+        upvotes: 0
+    },
+]
+
 const app = express();
-//MIDDLEware
-//Tell express server: when it receives a request that has a JSON body/payload, 
-// it is going to parse and automatically make that available to us on req.body
 app.use(express.json())
 
-app.get('/hello', (req, res) => {
-    //The response to the request
-    res.send('Hello!');
-})
+//name here is the name of the article we want to upvote
+app.put('/api/articles/:name/upvote', (req, res) => {
+    //logic for upvote article
+    // we need to define fake db, which contain how many upvote each articles has? 
+    // Later, we will replace this with MongoDB
 
-app.post('/hello', (req, res) => {
-    console.log(req.body)
-    res.send(`Hello ${req.body.name}!`);
+    // Based on the value of URL param, we need to know what article we need to upvote, 
+    //  and then we will increse value of upvotes of that article's object on our DB
+    const { name } = req.params;
+
+    //find the corresponding article with that name
+    const article = articlesInfo.find(article => article.name === name);
+    //make sure that article exists
+    if (article) {
+        article.upvotes += 1;
+        //Announce for us how many upvote that article currently have
+        res.send(`The $${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        //In case that article does not exist, we need to annouce to user that this article does not exist
+        res.send(`The ${name} article does not exists`);
+    }
+    
 })
 
 app.listen(8000, () => {
