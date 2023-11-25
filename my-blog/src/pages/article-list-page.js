a6d4cd6885cadd2246c207192d3b3784a5e776ff
drakenevadie19 <drakennievadje19@gmail.com>
2023-11-24 21:55:55 -0600

    New Change: Changing the way of fetching data to modular and make components usable, putting to new file.

--- my-blog/src/pages/article-list-page.js
+++ my-blog/src/pages/article-list-page.js
@@ -1,16 +1,12 @@
+import ArticleList from "../components/articles-list";
 import articles from "./article-content";
-import { Link } from "react-router-dom";
+
 
 const ArticleListPage = () => {
     return (
         <>
             <h1>List of Articles</h1>
-            {articles.map(article => (
-                <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
-                    <h3>{article.title}</h3>
-                    <p>{ article.content[0].substring(0,150) }...</p>
-                </Link>
-            ))}
+            <ArticleList articles={articles} />
         </>
         
     )
