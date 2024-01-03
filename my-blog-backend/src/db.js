8b88c8741835fc3d7a93215de54ff5ba6ff9e8be
drakenevadie19 <drakennievadje19@gmail.com>
2024-01-02 21:25:32 -0600

    New Change: Connect to MongoDB Atlas

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,8 @@ let db;
 
 async function connectToDB(callback) {
     // 27017 is default port for MongoDB
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    // const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.flb3538.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     db = client.db('react-blog-db');
